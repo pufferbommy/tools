d994ccb4209f1d799b64e348cefbfd2dc6f1b940
pufferbommy <pufferbommy@gmail.com>
2025-04-29 14:24:48 +0700

    feat: add navigation menu component to site header and update Logo component

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -1,6 +1,6 @@
-import { Fragment, useEffect, useState } from "react";
+import { Menu, Search } from "lucide-react";
 import { Link } from "@tanstack/react-router";
-import { ChevronDown, Menu, Search } from "lucide-react";
+import { Fragment, useEffect, useState } from "react";
 
 import Logo from "./Logo";
 import { cn } from "@/lib/utils";
@@ -16,9 +16,16 @@ import {
   CommandSeparator,
 } from "@/components/ui/command";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 export function SiteHeader() {
-  const [activeCategory, setActiveCategory] = useState("");
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
@@ -34,152 +41,103 @@ export function SiteHeader() {
   }, []);
 
   return (
-    <>
-      <header
-        onMouseLeave={() => setActiveCategory("")}
-        className="bg-background group sticky top-0 z-50 border-b border-dashed"
-      >
-        <div
-          className={cn(
-            "container mx-auto py-4 flex items-center justify-between",
-            activeCategory && "border-transparent"
-          )}
-        >
-          <div className="flex-1 flex">
-            <Link
-              onClick={() => setActiveCategory("")}
-              to="/"
-              className="inline-flex gap-2 items-center font-semibold"
-            >
-              <Logo />
-              รวมมิตรเครื่องมือ
-            </Link>
-          </div>
-          <nav className="hidden lg:flex">
+    <header className="bg-background group sticky top-0 z-50 border-b border-dashed">
+      <div className="container mx-auto py-4 flex items-center">
+        <div className="flex-1 flex">
+          <Link to="/" className="inline-flex gap-2 items-center font-semibold">
+            <Logo />
+            รวมมิตรเครื่องมือ
+          </Link>
+        </div>
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList>
             {TOOL_CATEGORIES.map((category) => (
-              <div key={category.url}>
-                <Button
-                  onMouseEnter={() => setActiveCategory(category.url)}
-                  asChild
-                  onClick={() => setActiveCategory("")}
-                  variant="ghost"
-                  aria-expanded={activeCategory === category.url}
-                  aria-haspopup={category.items.length > 0}
-                >
-                  <Link key={category.url} to={category.url}>
-                    {category.name}
-                    {category.items.length > 0 && (
-                      <ChevronDown
-                        className={cn(
-                          "transition-transform",
-                          activeCategory === category.url && "rotate-180"
-                        )}
-                        size={16}
-                      />
-                    )}
-                  </Link>
-                </Button>
-                {category.items.length > 0 && (
-                  <div
-                    className={cn(
-                      "hidden pb-4 bg-background absolute border-b top-full left-0 h-auto w-full",
-                      activeCategory === category.url && "group-hover:block"
-                    )}
-                  >
-                    <div className="container border-none mx-auto grid-cols-3 grid">
-                      {category.items.map((item) => (
-                        <Button
-                          onClick={() => setActiveCategory("")}
-                          variant="ghost"
-                          key={item.url}
-                          asChild
-                        >
-                          <Link to={item.url}>{item.title}</Link>
-                        </Button>
-                      ))}
-                    </div>
-                  </div>
-                )}
-              </div>
-            ))}
-          </nav>
-          <div className="flex-1 flex justify-end">
-            <Button
-              onClick={() => {
-                setIsSearchOpen(true);
-                setActiveCategory("");
-              }}
-              variant="ghost"
-              size="icon"
-            >
-              <Search />
-            </Button>
-            <CommandDialog open={isSearchOpen} onOpenChange={setIsSearchOpen}>
-              <CommandInput placeholder="ค้นหาเครื่องมือ..." />
-              <CommandList>
-                <CommandEmpty>ไม่พบเครื่องมือที่ค้นหา</CommandEmpty>
-                {TOOL_CATEGORIES.map((category, index) => (
-                  <Fragment key={category.url}>
-                    {index > 0 && <CommandSeparator />}
-                    <CommandGroup heading={category.name}>
-                      {category.items.map((item) => (
-                        <CommandItem asChild>
-                          <Link
-                            to={item.url}
-                            onClick={() => setIsSearchOpen(false)}
-                          >
+              <NavigationMenuItem key={category.url} value={category.url}>
+                <NavigationMenuTrigger>{category.name}</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    {category.items.map((item) => (
+                      <li key={item.url}>
+                        <NavigationMenuLink asChild>
+                          <a className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground text-sm font-medium">
                             {item.title}
-                          </Link>
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Fragment>
-                ))}
-              </CommandList>
-            </CommandDialog>
-            <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
-              <DrawerTrigger asChild>
-                <Button className="lg:hidden" variant="ghost" size="icon">
-                  <Menu />
-                </Button>
-              </DrawerTrigger>
-              <DrawerContent className="px-4">
-                {TOOL_CATEGORIES.map((category) => (
-                  <div key={category.url} className="flex flex-col">
+                          </a>
+                        </NavigationMenuLink>
+                      </li>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="flex-1 flex justify-end">
+          <Button
+            onClick={() => setIsSearchOpen(true)}
+            variant="ghost"
+            size="icon"
+          >
+            <Search />
+          </Button>
+          <CommandDialog open={isSearchOpen} onOpenChange={setIsSearchOpen}>
+            <CommandInput placeholder="ค้นหาเครื่องมือ..." />
+            <CommandList>
+              <CommandEmpty>ไม่พบเครื่องมือที่ค้นหา</CommandEmpty>
+              {TOOL_CATEGORIES.map((category, index) => (
+                <Fragment key={category.url}>
+                  {index > 0 && <CommandSeparator />}
+                  <CommandGroup heading={category.name}>
+                    {category.items.map((item) => (
+                      <CommandItem key={item.url} asChild>
+                        <Link
+                          to={item.url}
+                          onClick={() => setIsSearchOpen(false)}
+                        >
+                          {item.title}
+                        </Link>
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </Fragment>
+              ))}
+            </CommandList>
+          </CommandDialog>
+          <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+            <DrawerTrigger asChild>
+              <Button className="lg:hidden" variant="ghost" size="icon">
+                <Menu />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent className="px-4">
+              {TOOL_CATEGORIES.map((category) => (
+                <div key={category.url} className="flex flex-col">
+                  <Button
+                    asChild
+                    onClick={() => setIsDrawerOpen(false)}
+                    variant="ghost"
+                    className="font-semibold justify-start"
+                  >
+                    <Link key={category.url} to={category.url}>
+                      {category.name}
+                    </Link>
+                  </Button>
+                  {category.items.map((item) => (
                     <Button
-                      asChild
                       onClick={() => setIsDrawerOpen(false)}
                       variant="ghost"
-                      className="font-semibold justify-start"
+                      key={item.url}
+                      className="justify-start"
+                      asChild
                     >
-                      <Link key={category.url} to={category.url}>
-                        {category.name}
-                      </Link>
+                      <Link to={item.url}>{item.title}</Link>
                     </Button>
-                    {category.items.map((item) => (
-                      <Button
-                        onClick={() => setIsDrawerOpen(false)}
-                        variant="ghost"
-                        key={item.url}
-                        className="justify-start"
-                        asChild
-                      >
-                        <Link to={item.url}>{item.title}</Link>
-                      </Button>
-                    ))}
-                  </div>
-                ))}
-              </DrawerContent>
-            </Drawer>
-          </div>
+                  ))}
+                </div>
+              ))}
+            </DrawerContent>
+          </Drawer>
         </div>
-      </header>
-      <div
-        className={cn(
-          "bg-black/25 z-40 fixed inset-0 hidden",
-          activeCategory && "block"
-        )}
-      />
-    </>
+      </div>
+    </header>
   );
 }
