b56dc8b3a5d8f89b45a8572866735c56dd25597d
pufferbommy <pufferbommy@gmail.com>
2025-04-29 15:15:04 +0700

    fix: footer wording

--- app/components/SiteFooter.tsx
+++ app/components/SiteFooter.tsx
@@ -4,13 +4,14 @@ export default function SiteFooter() {
   return (
     <footer className="border-t border-dashed">
       <div className="container py-4 flex justify-between text-sm">
-        <p>
-          © {new Date().getFullYear()} รวมมิตรเครื่องมือ —
-          ขอบคุณที่ใช้เครื่องมือของเรา 💖
-        </p>
+        <p>© {new Date().getFullYear()} Ruammit Tools - All rights reserved</p>
         <nav>
-          <Button className="p-0 h-auto font-normal" variant="link" asChild>
-            <a href="/sitemap.xml">แผนผังเว็บไซต์</a>
+          <Button
+            className="p-0 h-auto font-normal text-inherit"
+            variant="link"
+            asChild
+          >
+            <a href="/sitemap.xml">Sitemap</a>
           </Button>
         </nav>
       </div>
