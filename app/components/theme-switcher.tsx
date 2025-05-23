802a3da32d03966f083fa522a15e961c3f0cc966
pufferbommy <pufferbommy@gmail.com>
2025-05-23 08:18:39 +0700

    fix: description in thai for toggle theme

--- app/components/theme-switcher.tsx
+++ app/components/theme-switcher.tsx
@@ -14,7 +14,7 @@ export default function ThemeSwitcher() {
 		<Button onClick={toggleTheme} variant="ghost" size="icon">
 			<Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 			<Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-			<span className="sr-only">Toggle theme</span>
+			<span className="sr-only">สลับธีม</span>
 		</Button>
 	);
 }
