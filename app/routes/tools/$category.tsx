6ac558e376af50262261f510133e94d94719c67d
pufferbommy <pufferbommy@gmail.com>
2025-05-26 17:02:15 +0700

    refactor: remove unused prop

--- app/routes/tools/$category.tsx
+++ app/routes/tools/$category.tsx
@@ -49,7 +49,7 @@ function RouteComponent() {
 		>
 			<div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
 				{category.tools.map((tool) => (
-					<ToolCard key={tool.url} tool={tool} noAnimation />
+					<ToolCard key={tool.url} tool={tool} />
 				))}
 			</div>
 		</ToolLayout>
